27552df02862a5e678501d0d2962877804184f8a
CoderJoeW <j.williamson@gambler-labs.com>
2023-03-07 23:37:38 -0500

    refactoring

--- Multi-Window SSH Client/Main.cs
+++ Multi-Window SSH Client/Main.cs
@@ -1,32 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
 using EasyTabs;
 
-namespace POME {
-    public partial class Main : Form { 
+namespace POME
+{
+    public partial class Main : Form
+    {
         private string host = "";
         private int port = 0;
         private string username = "";
         private string password = "";
-
         private readonly ErrorWindow error_window = new ErrorWindow();
 
-        protected TitleBarTabs ParentTabs {
-            get {
-                return (ParentForm as TitleBarTabs);
-            }
+        protected TitleBarTabs ParentTabs => (ParentForm as TitleBarTabs);
+
+        public Main(string args = "")
+        {
+            InitializeComponent();
+            if (!string.IsNullOrEmpty(args)) SetArgs(args);
         }
 
-        public Main(string args) {
-            InitializeComponent();
+        private void Main_Load(object sender, EventArgs e)
+        {
+            using (var form = new SessionDetails())
+            {
+                if (form.ShowDialog() == DialogResult.OK) SetSessionDetails(form);
+            }
+
+            sshTerminalControl1.AllowCopyingToClipboard = true;
+            sshTerminalControl1.AllowPastingFromClipboard = true;
+        }
+
+        private void ConnectToSSH()
+        {
+            try
+            {
+                sshTerminalControl1.Connect(host, port);
+                sshTerminalControl1.Authenticate(username, password);
+                sshTerminalControl1.Focus();
+                Text = host;
+            }
+            catch (Exception e) { DisplayError(e.Message); }
+        }
+
+        private void DisplayError(string msg)
+        {
+            error_window.SetErrorMessage(msg);
+            error_window.Show();
+        }
+
+        private void SetArgs(string args)
+        {
             host = args.Split('|')[0];
             port = int.Parse(args.Split('|')[1]);
             username = args.Split('|')[2];
@@ -34,101 +59,36 @@ namespace POME {
             ConnectToSSH();
         }
 
-        public Main() {
-            InitializeComponent();
-        }
-
-        private void Main_Load(object sender, EventArgs e) {
-            using(var form = new SessionDetails())
-            {
-                var result = form.ShowDialog();
-
-                if(result == DialogResult.OK)
-                {
-                    host = form.Host;
-                    port = form.Port;
-                    username = form.Username;
-                    password = form.Password;
-
-                    ConnectToSSH();
-                }
-            }
-
-            sshTerminalControl1.AllowCopyingToClipboard = true;
-            sshTerminalControl1.AllowPastingFromClipboard = true;
-        }
-
-        /*
-        --------------------------------
-                BEGIN METHODS
-        --------------------------------
-        */
-
-        #region Private
-        private void ConnectToSSH() {
-            try {
-                sshTerminalControl1.Connect(host, port);
-                sshTerminalControl1.Authenticate(username, password);
-                sshTerminalControl1.Focus();
-                this.Text = host;
-            }
-            catch (Exception e) {
-                DisplayError(e.Message);
-                //sshTerminalControl1.Disconnect();
-            }
-        }
-
-        private void DisplayError(string msg) {
-            error_window.SetErrorMessage(msg);
-            error_window.Show();
-        }
-        #endregion
-
-        #region Public
-
-        #endregion
-
-        /*
-        --------------------------------
-                END METHODS
-        --------------------------------
-        */
-
-        /*
-        --------------------------------
-                BEGIN LISTENERS
-        --------------------------------
-        */
-
-        #region Private
-        private void button1_Click(object sender, EventArgs e) {
+        private void SetSessionDetails(SessionDetails form)
+        {
+            host = form.Host;
+            port = form.Port;
+            username = form.Username;
+            password = form.Password;
             ConnectToSSH();
         }
 
-        private void form_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e) {
-            if (e.KeyCode == Keys.Enter) {
-                ConnectToSSH();
-            }
+        private void button1_Click(object sender, EventArgs e) => ConnectToSSH();
+
+        private void form_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) ConnectToSSH();
         }
 
-        private void ShowExtraFunctionsMenu(object sender, System.Windows.Forms.KeyEventArgs e) {
-            if (InputManager.CheckControlDown() && InputManager.CheckShiftDown() && Keyboard.IsKeyDown(Key.N)) {
-                DuplicateSession();
-            }
+        private void ShowExtraFunctionsMenu(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (InputManager.CheckControlDown() && InputManager.CheckShiftDown() && Keyboard.IsKeyDown(Key.N)) DuplicateSession();
         }
 
-        #endregion
+        private void PlaceholderRemoveText(object sender, EventArgs e) => (sender as TextBox).Text = "";
 
-        #region Public
-        public void PlaceholderRemoveText(object sender, EventArgs e) {
+        private void PlaceholderAddText(object sender, EventArgs e)
+        {
             TextBox tx = sender as TextBox;
-            tx.Text = "";
-        }
-
-        public void PlaceholderAddText(object sender,EventArgs e) {
-            TextBox tx = sender as TextBox;
-            if (string.IsNullOrWhiteSpace(tx.Text)) {
-                switch (tx.Name) {
+            if (string.IsNullOrWhiteSpace(tx.Text))
+            {
+                switch (tx.Name)
+                {
                     case "hostInfo":
                         tx.Text = "Host";
                         break;
@@ -147,40 +107,13 @@ namespace POME {
                 }
             }
         }
-        #endregion
 
-        /*
-        --------------------------------
-                END LISTENERS
-        --------------------------------
-        */
-
-        /*
-        --------------------------------
-                BEGIN COMMANDS
-        --------------------------------
-        */
-
-        #region Private
-        private void DuplicateSession() {
-            string args = host + "|" + port + "|" + username + "|" + password;
+        private void DuplicateSession()
+        {
+            string args = $"{host}|{port}|{username}|{password}";
             Main newMain = new Main(args);
-            Program.container.Tabs.Add(new TitleBarTab(Program.container) {
-
-                Content = newMain
-            });
+            Program.container.Tabs.Add(new TitleBarTab(Program.container) { Content = newMain });
             Program.container.SelectedTabIndex += 1;
         }
-        #endregion
-
-        #region Public
-
-        #endregion
-
-        /*
-        --------------------------------
-                END COMMANDS
-        --------------------------------
-        */
     }
 }
