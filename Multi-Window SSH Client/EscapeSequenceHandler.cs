b269ee1444c621ba0fbd78b82d9e3c423fd2e1c9
CoderJoeW <j.williamson@gambler-labs.com>
2023-03-15 20:52:20 -0400

    reducing size of code

--- Multi-Window SSH Client/EscapeSequenceHandler.cs
+++ Multi-Window SSH Client/EscapeSequenceHandler.cs
@@ -26,58 +26,37 @@ public class EscapeSequenceHandler
         return output;
     }
 
-    private static string HandleXTermEscapeSequences(string input, RichTextBox richTextBox, Form mainForm)
+    private static string HandleXTermEscapeSequences(string input, RichTextBox rtb, Form mainForm)
     {
-        StringBuilder output = new StringBuilder();
-        int currentIndex = 0;
+        var output = new StringBuilder();
+        int curIdx = 0;
+        string pat = @"\x1B(?:\[((?:\d{1,3};)*\d{1,3}[ABCDEFGHJKSTfhilmnprsu]|\?1(?:l|h)|\d{1,3}(?:[ABCD]|[su])|6n|[=c]|[=>]|[=>]|[8Cm]|[7D]|[10q]|[\?12l]|[8Cm]|[\?25l]|[\?25h])|\](\d*);(.*?)\x1B\\|Ptmux;(\x1B(?:\[[^P]*P)*)\x1B\\)";
 
-        string pattern = @"\x1B\[((?:\d{1,3};)*\d{1,3}[ABCDEFGHJKSTfhilmnprsu]|\?1(?:l|h)|\d{1,3}(?:[ABCD]|[su])|6n|[=c]|[=>]|[=>]|[8Cm]|[7D]|[10q]|[\?12l]|[8Cm]|[\?25l]|[\?25h])";
-        string oscPattern = @"\x1B\](\d*);(.*?)\x1B\\";
-        string tmuxPattern = @"\x1BPtmux;(\x1B(?:\[[^P]*P)*)\x1B\\";
-
-        pattern = pattern + "|" + oscPattern + "|" + tmuxPattern;
-
-        var matches = Regex.Matches(input, pattern);
-        foreach (Match match in matches)
+        Match m = Regex.Match(input, pat);
+        while (m.Success)
         {
-            int matchIndex = match.Index;
-            output.Append(input.Substring(currentIndex, matchIndex - currentIndex));
+            int mIdx = m.Index;
+            output.Append(input.Substring(curIdx, mIdx - curIdx));
 
-            string sequence = match.Groups[1].Value;
-            string oscSequence = match.Groups[2].Value;
-            string tmuxSequence = match.Groups[3].Value;
+            string seq = m.Groups[1].Value, oscSeq = m.Groups[2].Value, tmuxSeq = m.Groups[4].Value;
 
-            if (!string.IsNullOrEmpty(sequence))
-            {
-                ApplyXTermSequence(sequence, richTextBox);
-            }
-            else if (!string.IsNullOrEmpty(oscSequence))
-            {
-                HandleOscSequence(oscSequence, mainForm);
-            }
-            else if (!string.IsNullOrEmpty(tmuxSequence))
-            {
-                HandleTmuxSequence(tmuxSequence, richTextBox);
-            }
+            if (!string.IsNullOrEmpty(seq)) ApplyXTermSequence(seq, rtb);
+            else if (!string.IsNullOrEmpty(oscSeq)) HandleOscSequence(oscSeq, mainForm);
+            else if (!string.IsNullOrEmpty(tmuxSeq)) HandleTmuxSequence(tmuxSeq, rtb);
 
-            currentIndex = matchIndex + match.Length;
+            curIdx = mIdx + m.Length;
+            m = m.NextMatch();
         }
 
-        if (currentIndex < input.Length)
+        if (curIdx < input.Length)
         {
-            bool isBracketedPasteModeEnabled = richTextBox.Tag is bool isEnabled && isEnabled;
+            bool isBPMEnabled = rtb.Tag is bool isEnabled && isEnabled;
 
-            if (isBracketedPasteModeEnabled)
-            {
-                output.Append("\x1B[200~"); // Bracketed paste mode start sequence
-            }
+            if (isBPMEnabled) output.Append("\x1B[200~"); // BPM start sequence
 
-            output.Append(input.Substring(currentIndex));
+            output.Append(input.Substring(curIdx));
 
-            if (isBracketedPasteModeEnabled)
-            {
-                output.Append("\x1B[201~"); // Bracketed paste mode end sequence
-            }
+            if (isBPMEnabled) output.Append("\x1B[201~"); // BPM end sequence
         }
 
         return output.ToString();
@@ -92,119 +71,64 @@ public class EscapeSequenceHandler
 
     private static void ApplyXTermSequence(string sequence, RichTextBox richTextBox)
     {
-        // Implement the logic to apply XTerm escape sequences
         string[] parameters = sequence.Split(';');
         char command = sequence[sequence.Length - 1];
-        int value;
         int parsedParam;
+
+        int GetValue(int defaultValue = 1) =>
+            parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : defaultValue;
 
         switch (command)
         {
-            case 'A': // Cursor up
-                value = parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : 1;
-                CursorUp(richTextBox, value);
-                break;
-            case 'B': // Cursor down
-                value = parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : 1;
-                CursorDown(richTextBox, value);
-                break;
-            case 'C': // Cursor forward (right)
-                value = parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : 1;
-                CursorForward(richTextBox, value);
-                break;
-            case 'D': // Cursor backward (left)
-                value = parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : 1;
-                CursorBackward(richTextBox, value);
-                break;
-            case 'H': // Cursor position (row and column)
+            case 'A': CursorUp(richTextBox, GetValue()); break;
+            case 'B': CursorDown(richTextBox, GetValue()); break;
+            case 'C': CursorForward(richTextBox, GetValue()); break;
+            case 'D': CursorBackward(richTextBox, GetValue()); break;
+            case 'H':
             case 'f':
                 if (parameters.Length == 2 && int.TryParse(parameters[0], out parsedParam) && int.TryParse(parameters[1], out int parsedParam2))
                 {
                     SetCursorPosition(richTextBox, parsedParam - 1, parsedParam2 - 1);
                 }
                 break;
-            case 'S': // Scroll up
-                value = parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : 1;
-                ScrollUp(richTextBox, value);
-                break;
-            case 'T': // Scroll down
-                value = parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : 1;
-                ScrollDown(richTextBox, value);
-                break;
-            case 'm': // Select Graphic Rendition (SGR)
-                for (int i = 0; i < parameters.Length; i++)
-                {
-                    if (int.TryParse(parameters[i], out parsedParam))
-                    {
-                        if (parsedParam >= 30 && parsedParam <= 37) // Set foreground color (basic 8 colors)
-                        {
-                            richTextBox.SelectionColor = GetColorFromSGRCode(parsedParam);
-                        }
-                        else if (parsedParam >= 90 && parsedParam <= 97) // Set bright foreground color
-                        {
-                            richTextBox.SelectionColor = GetColorFromSGRCode(parsedParam);
-                        }
-                        else if (parsedParam >= 40 && parsedParam <= 47) // Set background color (basic 8 colors)
-                        {
-                            richTextBox.SelectionBackColor = GetColorFromSGRCode(parsedParam);
-                        }
-                        else if (parsedParam >= 100 && parsedParam <= 107) // Set bright background color
-                        {
-                            richTextBox.SelectionBackColor = GetColorFromSGRCode(parsedParam);
-                        }
-                        else if (parsedParam == 0) // Reset all attributes
-                        {
-                            richTextBox.SelectionColor = Color.White;
-                            richTextBox.SelectionBackColor = Color.Black;
-                        }
-                        else if (parsedParam == 38 || parsedParam == 48) // Extended colors
-                        {
-                            Color extendedColor = GetExtendedColor(parameters[i], parameters);
-                            if (extendedColor != Color.Empty)
-                            {
-                                if (parsedParam == 38)
-                                {
-                                    richTextBox.SelectionColor = extendedColor;
-                                }
-                                else
-                                {
-                                    richTextBox.SelectionBackColor = extendedColor;
-                                }
-                            }
-                        }
-                        // Add more cases for other SGR parameters as needed
-                    }
-                }
-                break;
-            case 'J': // Erase in Display (ED)
-                value = parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : 0;
-                EraseInDisplay(richTextBox, value);
-                break;
-            case '@': // Insert blank characters
-                value = parameters.Length > 0 && int.TryParse(parameters[0], out parsedParam) ? parsedParam : 1;
-                InsertBlankCharacters(richTextBox, value);
-                break;
-            case '[': // Private Mode Set (DECSET)
-                if (sequence.StartsWith("?2004"))
-                {
-                    if (sequence.EndsWith("h")) // Enable Bracketed Paste Mode
-                    {
-                        richTextBox.Tag = true;
-                    }
-                    else if (sequence.EndsWith("l")) // Disable Bracketed Paste Mode
-                    {
-                        richTextBox.Tag = false;
-                    }
-                }
-                break;
-
-            // Add and update cases for additional XTerm escape sequences
-
-            default:
-                // Handle other XTerm escape sequences or ignore unknown ones
-                break;
+            case 'S': ScrollUp(richTextBox, GetValue()); break;
+            case 'T': ScrollDown(richTextBox, GetValue()); break;
+            case 'm': ApplySGRParameters(richTextBox, parameters); break;
+            case 'J': EraseInDisplay(richTextBox, GetValue(0)); break;
+            case '@': InsertBlankCharacters(richTextBox, GetValue()); break;
+            case '[': ApplyPrivateModeSet(sequence, richTextBox); break;
+            default: break; // Handle other XTerm escape sequences or ignore unknown ones
         }
     }
+
+    private static void ApplySGRParameters(RichTextBox rtb, string[] parameters)
+    {
+        for (int i = 0; i < parameters.Length; i++)
+    {
+            if (int.TryParse(parameters[i], out int p))
+        {
+                rtb.SelectionColor = (p >= 30 && p <= 37) || (p >= 90 && p <= 97) ? GetColorFromSGRCode(p) : p == 0 ? Color.White : p == 38 ? GetExtendedColor (parameters[i], parameters) : rtb.SelectionColor;
+                rtb.SelectionBackColor = (p >= 40 && p <= 47) || (p >= 100 && p <= 107) ? GetColorFromSGRCode(p) : p == 0 ? Color.Black : p == 48 ? GetExtendedColor (parameters[i], parameters) : rtb.SelectionBackColor;
+            }
+        }
+    }
+
+
+    private static void ApplyPrivateModeSet(string sequence, RichTextBox richTextBox)
+    {
+        if (sequence.StartsWith("?2004"))
+        {
+            if (sequence.EndsWith("h")) // Enable Bracketed Paste Mode
+            {
+                richTextBox.Tag = true;
+            }
+            else if (sequence.EndsWith("l")) // Disable Bracketed Paste Mode
+            {
+                richTextBox.Tag = false;
+            }
+        }
+    }
+
 
     private static void CursorUp(RichTextBox richTextBox, int lines)
     {
@@ -391,37 +315,19 @@ public class EscapeSequenceHandler
 
     private static Color GetColorFrom256ColorPalette(int index)
     {
-        if (index < 0 || index >= 256)
-        {
-            return Color.Empty;
-        }
+        if (index < 0 || index >= 256) return Color.Empty;
 
-        if (index < 16)
+        if (index < 16) return GetColorFromBasic16ColorPalette(index);
+
+        if (index < 232)
         {
-            // 16 basic colors
-            return GetColorFromBasic16ColorPalette(index);
-        }
-        else if (index < 232)
-        {
-            // 216 colors (6x6x6 RGB color cube)
             index -= 16;
-            int red = index / 36;
-            int green = (index % 36) / 6;
-            int blue = index % 6;
-
-            // Convert to RGB values
-            red = red * 51;
-            green = green * 51;
-            blue = blue * 51;
-
+            int red = index / 36 * 51, green = (index % 36) / 6 * 51, blue = index % 6 * 51;
             return Color.FromArgb(red, green, blue);
         }
-        else
-        {
-            // 24 grayscale colors
-            int gray = (index - 232) * 10 + 8;
-            return Color.FromArgb(gray, gray, gray);
-        }
+
+        int gray = (index - 232) * 10 + 8;
+        return Color.FromArgb(gray, gray, gray);
     }
 
     private static Color GetColorFromBasic16ColorPalette(int index)
@@ -448,6 +354,4 @@ public class EscapeSequenceHandler
             default: return Color.Empty;
         }
     }
-
 }
-
